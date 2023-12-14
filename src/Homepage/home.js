061b4202bb4265bd864b44fdf8cfced204c2b50f
menth0s <marslite@pm.me>
2023-12-14 00:42:21 -0500

    rolling back lol

--- src/Homepage/home.js
+++ src/Homepage/home.js
@@ -19,26 +19,22 @@ function HomePage() {
         let updateDomains = [...domains];
 
         if (domainOne !== "") {
-            // Splitting domainOne into an array and removing 'https://www.' from each element
-            const domainOneArray = domainOne.split(" ").map(domain =>
-                domain.replace(/https:\/\/www\./gi, "").toLowerCase()
-            );
+            // Splitting domainOne into an array and removing 'https://www.' from each element   
+            const domainOneRefined = domainOne.toLowerCase();
+            const conformURL = domainOneRefined
+                .replace(/^https?:\/\//, "")
+                .replace(/^www\./, "")
+                .replace("/", "")
+                .toLowerCase();
+            updateDomains = [...updateDomains, conformURL];
 
             // Adding each processed domain from domainOneArray to updateDomains
-            updateDomains = [...updateDomains, ...domainOneArray];
-
+    
             console.log(domainOneArray);
             console.log(domainOne);
             // console.log('test')
 
-            //Storing all the submitted domains inside domains[]    
-            // const domainOneRefined = domainOne.toLowerCase();
-            // const conformURL = domainOneRefined
-            //     .replace(/^https?:\/\//, "")
-            //     .replace(/^www\./, "")
-            //     .replace("/", "")
-            //     .toLowerCase();
-            // updateDomains = [...updateDomains, conformURL];
+
 
             setDomains(updateDomains);
         }
